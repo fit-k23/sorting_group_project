296d9e2eb108b6574f403fa006d8da23e846d473
NTT1906 <nttispay@gmail.com>
2024-06-14 16:25:30 +0700

    ADD: selection_sort

---
+++ src/sort/selection_sort.cpp
@@ -0,0 +1,18 @@
+#include "../utils/utils.h"
+
+Result selectionSort(int *a, int n) {
+	Result r;
+	auto start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; ++r.cmps && i < n - 1; i++) {
+		int cur = i;
+		for (int j = i + 1; ++r.cmps && j < n; j++) {
+			if (++r.cmps && a[j] < a[cur]) cur = j;
+		}
+
+		if (++r.cmps && cur != i) Swap(a[cur], a[i]);
+	}
+	std::chrono::duration<double, std::milli> duration = std::chrono::high_resolution_clock::now() - start;
+	//(std::chrono::high_resolution_clock::now() - start).count();
+	r.time = duration.count();
+	return r;
+}
